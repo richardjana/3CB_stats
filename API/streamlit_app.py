5ab8988e37de47055128acd4db55b080ca65d149
richardjana <richard.c.jana@gmail.com>
2025-12-05 13:16:46 +0200

    attempt to compile the data on the fly

--- API/streamlit_app.py
+++ API/streamlit_app.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from glob import glob
 
 import pandas as pd
 import plotly.graph_objects as go
@@ -9,8 +9,36 @@ import streamlit as st
 st.title('PvP match history')
 
 @st.cache_data
-def load_data() -> Tuple[pd.DataFrame, List[str]]:
-    data = pd.read_csv('data/pvp_match_data.csv', sep=';')
+def load_data() -> tuple[pd.DataFrame, list[str]]:
+    def get_transform_round_df(csv_file: str) -> pd.DataFrame:
+        """ Get df for a single ronud and transform to match-centric format. """
+        df = pd.read_csv(csv_file, sep=';')
+
+        round = df['round'].iloc[0]
+        df = df.drop('round', axis=1)
+        df = df.drop([col for col in df.columns if col.startswith('card_')], axis=1)
+
+        player_dict = dict(zip([col for col in df.columns if col.startswith('result_')],
+                               df['player'].tolist()))
+        df = df.rename(columns=player_dict)
+
+        df_long = df.melt(id_vars='player', var_name='opponent', value_name='points_player')
+        df_long = df_long[df_long['player'] != df_long['opponent']]
+
+        df_matches = df_long.merge(df_long,
+                                   left_on=['player', 'opponent'],
+                                   right_on=['opponent', 'player'],
+                                   suffixes=('_1', '_2'))
+        df_matches = df_matches.drop(['opponent_1', 'opponent_2'], axis=1)
+        df_matches['round'] = round
+
+        return df_matches
+
+    round_dfs = []
+    for csv in natsorted(glob('../API/data/raw/round_*.csv')):
+        round_dfs.append(get_transform_round_df(csv))
+
+    data = pd.concat(round_dfs)
     all_players = natsorted(data['player_1'].unique(), alg=ns.IGNORECASE)
 
     return data, all_players
